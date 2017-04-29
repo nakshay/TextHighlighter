5097118ea7b0932e697989422bd604653103b098
Akshay Naik <akshaynaik93@gmail.com>
2017-04-29 18:50:14 +0530

    new interfaces implemented

--- texthighlighterapi/src/main/java/com/github/akshay_naik/texthighlighterapi/LibraryConstants.java
+++ texthighlighterapi/src/main/java/com/github/akshay_naik/texthighlighterapi/LibraryConstants.java
@@ -4,8 +4,26 @@ package com.github.akshay_naik.texthighlighterapi;
  * Created by akshay on 29/04/17.
  */
 
-public interface Constants {
+public interface LibraryConstants {
+
     public interface Language {
+
+        String JAVA="JAVA";
+        String C="C";
+        String CPP="C++";
+    }
+
+    public interface Style {
+
+        //pre-defined Styles
+
+        public static final String NORMAL="NORMAL";
+        public static final String BOLD="BOLD";
+        public static final String ITALIC="ITALIC";
+        public static final String UNDERLINE="UNDERLINE";
+        public static final String SUPERSCRIPT="SUPERSCRIPT";
+        public static final String SUBSCRIPT="SUBSCRIPT";
+
 
     }
 }
