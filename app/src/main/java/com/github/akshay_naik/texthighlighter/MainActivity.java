e5a93c080de4d4a07d5d319ede7b5d70c7c51591
Akshay Naik <akshaynaik93@gmail.com>
2017-05-01 12:08:37 +0530

    Language selection testing

--- app/src/main/java/com/github/akshay_naik/texthighlighter/MainActivity.java
+++ app/src/main/java/com/github/akshay_naik/texthighlighter/MainActivity.java
@@ -22,25 +22,27 @@ public class MainActivity extends AppCompatActivity {
 
         TextHighlighter highlighter=new TextHighlighter();
 
-        highlighter.setDefaultColor("black");
+        String code="#include  &lt; iostream.h > <br/>  <br/> void main (  )  <br/>  {  <br/> &nbsp;&nbsp; int s1, s2, s3, s4, s5, sum, total = 500; <br/> &nbsp;&nbsp; float per; <br/> &nbsp;&nbsp; cout &lt;&lt; \"\\nEnter marks of 5 subjects :\"; <br/> &nbsp;&nbsp; cin >>s1>>s2>>s3>>s4>>s5; <br/> &nbsp;&nbsp;  <br/> &nbsp;&nbsp; sum = s1 + s2 + s3 + s4 + s5; <br/> &nbsp;&nbsp; cout &lt;&lt; \"\\nSum :\" &lt;&lt; sum; <br/> &nbsp;&nbsp;  <br/> &nbsp;&nbsp; per =  ( sum * 100 ) /500; <br/> &nbsp;&nbsp; /* percentage formula*/ <br/> &nbsp;&nbsp; cout &lt;&lt; \"\\nPercentage :\" &lt;&lt; per; <br/>  }  <br/>  <br/> Output: <br/>  <br/> Enter marks of % subjects : <br/> 89 81 79 85 75 <br/> sum: 409 <br/> Percentage: 81.00000 <br/> ";
 
-       /* highlighter.setColorForTheToken("awesome","red");
+/*        highlighter.setDefaultColor("black");
+
+        highlighter.setColorForTheToken("awesome","red");
         highlighter.setColorForTheToken("text","purple");
         highlighter.setColorForTheToken("highlighter","blue");
-
+*/
         highlighter.setStyleForTheToken("Awesome",TextHighlighter.BOLD);
         highlighter.setStyleForTheToken("Text",TextHighlighter.UNDERLINE);
         highlighter.setStyleForTheToken("Highlighter",TextHighlighter.ITALIC);
 
-*/
-       highlighter.setLanguage(highlighter.C);
-        mTextView= (TextView) findViewById(R.id.mytextview);
-        mEditText =(EditText) findViewById(R.id.myedittext);
 
-        String highlightedText=highlighter.getHighlightedText("Awesome text highlighter");
+       highlighter.setLanguage(highlighter.JAVA);
+        mTextView= (TextView) findViewById(R.id.mytextview);
+
+
+        String highlightedText=highlighter.getHighlightedText(code);
         String styledText=highlighter.getStyledText(highlightedText);
 
         mTextView.setText(Html.fromHtml(highlightedText));
-        mEditText.setText(Html.fromHtml(styledText));
+
     }
 }
